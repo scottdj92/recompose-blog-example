57528e45c66088eca45692f0319a6778523e2d4a
Scott Jones <scottdj92@gmail.com>
2018-04-15 00:09:49 -0400

    Add right branch to whileLoading

--- whileLoading.tsx
+++ whileLoading.tsx
@@ -3,6 +3,7 @@ import { branch, renderNothing, renderComponent } from "recompose";
 const whileLoading = () => WrappedComponent => branch(
     (props) => props !== undefined,
     renderComponent(WrappedComponent),
+    renderNothing,
 );
 
 export default whileLoading;
